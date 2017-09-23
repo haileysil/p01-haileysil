19ea9a621a601179e6f57d8c416ece86698ed4fc
haileysil <hsilveira1@mail.csuchico.edu>
2017-09-23 00:19:04 -0700

    asks user if they would like to play again

--- guess.cpp
+++ guess.cpp
@@ -6,60 +6,67 @@ using namespace std;
 
 int main ()
 {
-srand ( time (NULL) );
-int randomNumber;
-int range;
-int input, num_of_turns;
-string difficulty;
+        srand ( time (NULL) );
+        int randomNumber;
+        int range;
+        int input, num_of_turns;
+        string difficulty;
+        char answer;
+        do 
+        {
+                cout<<"Please select a difficulty level: easy, medium, or hard:"<<endl;
+                cin>>difficulty;
 
-cout<<"Please select a difficulty level: easy, medium, or hard:"<<endl;
-cin<<difficulty;
+                if (difficulty=="easy")
+                {
+                        range = 2;
+                        num_of_turns = 1;
+                }
 
-if (difficulty=="easy")
-{
-range = 2;
-num_of_turns = 1;
-}
+                else if (difficulty=="medium")
+                {
+                        range = 10;
+                        num_of_turns = 2;
+                }
 
-else if (difficulty=="medium")
-{
-range = 10;
-num_of_turns = 2;
-}
+                else if (difficulty=="hard")
+                {
+                        range = 100;
+                        num_of_turns = 3;
+                }
 
-else if (difficulty=="hard")
-{
-range = 100;
-num_of_turns = 3;
-}
+                else
+                {
+                        cout<<"Error! You did not enter a difficulty level."<<endl;
+                        return 0;
+                }
 
-else
-{
-cout<<"Error! You did not enter a difficulty level."<<endl;
-return 0;
-}
+                randomNumber = rand() % range + 1;
+                bool winner = false;
 
-randomNumber = rand() range % + 1;
+                do 
+                {
+                        cout<<"Guess the computer's number: "<<endl;
+                        cin>>input;
 
-do 
-{
-cout<<"Guess the computer's number: "<<endl;
-cin>>input;
+                        if (input==randomNumber)
+                        {
+                                cout<<"You, guessed correctly! You win!"<<endl;
+                                winner = true;
+                        }
+                        else if (randomNumber > input)
+                        {
+                                cout<<"Your number is too low." <<endl;
+                        }
+                        else if (randomNumber < input)
+                        {
+                                cout<<"Your number is too high." <<endl;
+                        }
+                        num_of_turns--;
+                } while ((num_of_turns > 0)&&(winner == false));
 
-if (input==randomNumber)
- {
- cout<<"You, guessed correctly! You win!"<<endl;
- }
-else if (randomNumber > input)
-  {
-  cout<<"Your number is too low." <<endl;
-  }
-  else if (randomNumber < input)
-  {
-  cout<<"Your number is too high." <<endl;
-  }
- num_of_turns--;
- } while (num_of_turns>0);
-
-return 0;
+                cout<<"The game is over. Would you like to play again? (y/n)"<<endl;
+                cin>>answer;
+        } while (answer=='y');
+        return 0;
 }
